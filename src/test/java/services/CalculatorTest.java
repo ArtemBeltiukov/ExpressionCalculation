72b4470e560913d6c7c3b7b8bde86a8a23ac23d0
Artem Beltiukov <Artem_Beltiukov@epam.com>
2021-03-25 16:30:50 +0400

    fix

--- src/test/java/services/CalculatorTest.java
+++ src/test/java/services/CalculatorTest.java
@@ -3,8 +3,12 @@ package services;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -19,35 +23,41 @@ class CalculatorTest {
     double value4 = 4;
     double value5 = 5;
 
+    private static Stream<Arguments> provideIntsForDivisionByZero() {
+        return Stream.of(
+                Arguments.of(1, 0)
+        );
+    }
+
     @Test
-    void addition() {
+    void additionTest() {
         assertThat( value1 + value2, equalTo(calculator.addition(value1,value2)));
     }
 
     @Test
-    void subtraction() {
+    void subtractionTest() {
         assertThat(value1 - value2,equalTo(calculator.subtraction(value1,value2)));
     }
 
     @Test
-    void multiplication() {
+    void multiplicationTest() {
         assertThat(value1 * value2,equalTo(calculator.multiplication(value1,value2)));
     }
 
     @Test
-    void division() {
+    void divisionTest() {
         assertThat(value1 / value2,equalTo(calculator.division(value1,value2)));
     }
 
 
     @ParameterizedTest
-    @CsvSource({"1,0"})
-    void divisionByZero(int v1, int v2 ) {
+    @MethodSource("provideIntsForDivisionByZero")
+    void divisionByZeroTest(int v1, int v2 ) {
         Assert.assertThrows(IllegalArgumentException.class,()->calculator.division(v1,v2));
     }
 
     @Test
-    void calculateExpression() {
+    void calculateExpressionTest() {
         String test = value1 + " / " + value2 + " + " + value3 + " - " +value4 + " * " + value5;
         assertThat((value1/value2) + value3 - (value4 * value5),equalTo(calculator.calculateExpression(test)));
     }
